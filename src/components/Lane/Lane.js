5b593006aa582a6dc2240a393126b3c25745cc9b
Collins Mr <96517341+Cokode@users.noreply.github.com>
2023-07-07 13:51:55 +0200

    removed Lane.css, added styled-components

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,10 +1,43 @@
 import Task from  '../../task/Task';
-import './Lane.css';
+//import './Lane.css';
+import styled from 'styled-components'
 
-function Lane({ title, loading, error, tasks, onDragStart, onDragOver }) {
+const LaneWrapper = styled.div`
+  text-alignL left;
+  padding: 0;
+  background: lightGray;
+  border-radius: 20px;
+  min-height: 50vh;
+  width 20vw;
+;
+
+@media (max-width: 768px) {
+  margin-bottom: 5%;
+}
+`;
+
+const Title = styled.h2`
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 1px solid darkGray;
+`;
+
+
+function Lane({laneId, 
+  title, 
+  loading, 
+  error, 
+  tasks,
+  onDragStart, 
+  onDragOver,
+  onDrop })
+    {
+
   return (
-    <div className='Lane-wrapper' onDragOver = {onDragOver}>
-      <h2>{title}</h2>
+    <LaneWrapper onDragOver = {onDragOver}
+    onDrop={(e) => onDrop(e, laneId)}>
+      <Title>{title}</Title>
       {loading || error?
       (<span>
         {error || 'loading...'}
@@ -19,7 +52,7 @@ function Lane({ title, loading, error, tasks, onDragStart, onDragOver }) {
           />
         ))
       )}
-    </div>
+    </LaneWrapper>
   );
 }
 
